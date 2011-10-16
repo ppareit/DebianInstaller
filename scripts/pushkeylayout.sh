59e79318dece66f4b01863c9af56ba6d857949fd
Pieter Pareit <pieter.pareit@gmail.com>
2011-10-16 18:27:26 +0200

    KL: Improved keyboard layout and keychars

--- scripts/pushkeylayout.sh
+++ scripts/pushkeylayout.sh
@@ -1,10 +1,10 @@
 #!/bin/sh
 
-if [ $# -ne 2]
+if [ $# -ne 2 ]
 then
     echo "Usage - $0 device layout"
     echo "           Where device currently is only TF101 and layout currently only is BE"
-    echo "           Connact me to support more devices"
+    echo "           Contact me to support more devices"
     exit 1
 fi
 
@@ -12,13 +12,21 @@ case $1 in
     TF101)
 	case $2 in
 	    BE)
-		echo "Remounting /system as rw"
+		echo "Remounting /system as read/write"
 		adb shell su -c 'mount -o remount,rw -t yaffs2 /dev/block/mmcblk0p1 /system'
 		echo "Pushing keylayout to device"
-		adb push ../data/transformer/keylayout/asusec.kl.be /system/usr/keylayout/asusec.kl
+		adb push ../data/keylayouts/TF101/BE/asusec.kl /system/usr/keylayout/asusec.kl
+		echo "Pushing keychars to device"
+		adb push ../data/keychars/TF101/BE/asusec.kcm /system/usr/keychars/asusec.kcm
+		echo "Remounting system as read only"
+		adb shell su -c 'mount -o remount,ro -t yaffs2 /dev/block/mmcblk0p1 /system'
+		;;
+	esac
 	;;
     *)
 	echo "Unknown device"
 	exit 1
 	;;
 esac
+
+echo "For the changes to have effect, you have to reboot the device."
